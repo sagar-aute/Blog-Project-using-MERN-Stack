94964d2122fe6bbe1b537f94ae72dc29daa4c977
sagar-aute <sagaraute090@gmail.com>
2022-12-16 13:31:18 +0530

    "Created the app component"

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//pages
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="bg-red-500 text-center text-white">App component</div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>   
   );
 }
 
