d7111a4e41f96223269e98dff3629055d6fd088f
sagar-aute <sagaraute090@gmail.com>
2022-12-16 13:43:51 +0530

    "Creating Blog Pages"

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //pages
 import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticleList from "./pages/ArticleList";
+import Article from "./pages/Article";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/articles-list' element={<ArticleList />} />
+          <Route path='/article' element={<Article />} />        
         </Routes>
       </div>
     </Router>   
---
+++ client/src/pages/About.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const About = () => {
+    return (
+    <div className='mb-20 '>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+            About
+        </h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum
+        has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a
+        lgalley of type and scrambled it to make a type
+        specimen book. It has survived not only five
+        centuries, but also the leap into electronic
+        pesetting, remaining essentially unchanged. It
+        as popularised in the 1960s with the release of
+        (Mosterton TAIN) ROC TBA [OU TDR OY Ssct= soo
+        land more recently with desktop publishing
+        software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum
+        has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a
+        lgalley of type and scrambled it to make a type
+        specimen book. It has survived not only five
+        centuries, but also the leap into electronic
+        pesetting, remaining essentially unchanged. It
+        as popularised in the 1960s with the release of
+        (Mosterton TAIN) ROC TBA [OU TDR OY Ssct= soo
+        land more recently with desktop publishing
+        software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum
+        has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a
+        lgalley of type and scrambled it to make a type
+        specimen book. It has survived not only five
+        centuries, but also the leap into electronic
+        pesetting, remaining essentially unchanged. It
+        as popularised in the 1960s with the release of
+        (Mosterton TAIN) ROC TBA [OU TDR OY Ssct= soo
+        land more recently with desktop publishing
+        software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+
+        Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry. Lorem Ipsum
+        has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a
+        lgalley of type and scrambled it to make a type
+        specimen book. It has survived not only five
+        centuries, but also the leap into electronic
+        pesetting, remaining essentially unchanged. It
+        as popularised in the 1960s with the release of
+        (Mosterton TAIN) ROC TBA [OU TDR OY Ssct= soo
+        land more recently with desktop publishing
+        software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        </p>
+    </div>
+    )
+};
+
+export default About;
