1a32960770f446995c47fcca78565f8f3a607037
sagar-aute <sagaraute090@gmail.com>
2022-12-16 15:06:01 +0530

    "URL parameters with react-router"

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/articles-list' element={<ArticleList />} />
-          <Route path='/article' element={<Article />} />        
+          <Route path='/article/:name' element={<Article />} />        
         </Routes>
       </div>
     </Router>   
